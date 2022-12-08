abf22944c0aae4934b317b5fb3fe55a6c4ede019
santoshki <santoshkdevp@gmail.com>
2022-12-08 15:03:41 +0530

    Modified UI to include compress button

--- entity/compressor_ui.py
+++ entity/compressor_ui.py
@@ -6,11 +6,18 @@ class MyFrame(wx.Frame):
     def __init__(self):
         super().__init__(parent=None, title='Fx_Image Compressor Alphav1.0')
         panel = wx.Panel(self)
-        image_upload_button = wx.Button(panel, label='Upload Image', pos=(150, 150))
-        image_upload_button.Bind(wx.EVT_BUTTON, self.on_click())
+        self.browse_image_file_label = wx.StaticText(panel, id=1, label="Old Label.", pos=(150, 50),
+                                                     size=wx.DefaultSize, style=0, name="statictext")
+        self.browse_image_file_label.SetLabel("Browse Image file")
+        self.image_browse_button = wx.Button(panel, label='Browse', pos=(25, 100))
+        self.textbox = wx.TextCtrl(panel, size=(250, -1), pos=(100, 100))
+        self.compress_action_button = wx.Button(panel, label='Compress', pos=(150, 150))
+        self.progress_bar = wx.Gauge(panel, size=(150, 25), pos=(200, 300))
+        self.image_browse_button.Bind(wx.EVT_BUTTON, self.on_click_image_browse)
+        self.compress_action_button.Bind(wx.EVT_BUTTON, self.on_click_compress)
         self.Show()
 
-    def on_click(self, event):
+    def on_click_image_browse(self, event):
         try:
             print("Image upload button pressed.\nOpening File selection dialog box...")
             select_file_dialog_ui = wx.FileDialog(frame, "Open", "", "", "", wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
@@ -26,6 +33,11 @@ class MyFrame(wx.Frame):
         except Exception as e:
             print("Exception occurred:", e)
 
+    def on_click_compress(self, event):
+        try:
+            pass
+        except Exception as e:
+            print("Exception occurred:", e)
 
 if __name__ == '__main__':
     app = wx.App()
